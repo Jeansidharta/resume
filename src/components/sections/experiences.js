32a38a78a87832cc5b0effe94db29253f7cc8851
Jean Sidharta <jeansidharta@gmail.com>
2022-07-25 15:09:11 -0300

    Added TechViz and tweaked some text on "Experiences"

--- src/components/sections/experiences.js
+++ src/components/sections/experiences.js
@@ -16,7 +16,26 @@ const Italic = ({ children }) => <span className='italic'>{children}</span>
 const experiences = [
 	{
 		title: {
-			'pt-br': <>Desenvolvedor web júnior no <Italic>Pronto Combustíveis</Italic></>,
+			'pt-br': <>Reponsável de Tecnologia na <Italic>TechViz</Italic></>,
+		},
+		duration: {
+			'pt-br': <>2020 - hoje</>,
+		},
+		description: {
+			'pt-br': <>
+				<Italic>Tech Viz</Italic> é uma startup de solução de dados para diagnósticos de
+				Diversidade, Equidade e Inclusão (DE&I). Participo dessa empresa desde a sua formação,
+				projetando e gerenciando todas as necessidade de tecnologia, tais como o desenvolvimento
+				de seu produto de censo e de sua <Italic>Landing Page</Italic>. Esses projetos foram construidos
+				usando React com Nextjs em Typescript e foram implantados na AWS usando Terraform. Além disso,
+				sou responsável por determinar os requisitos de negócio a partir das demandas da equipe e dar
+				suporte para quaisquer problemas ligados aos projetos.
+			</>,
+		},
+	},
+	{
+		title: {
+			'pt-br': <>Desenvolvedor web no <Italic>Pronto Combustíveis</Italic></>,
 			'en-us': <>Junior web developer at <Italic>Pronto Combustíveis</Italic></>,
 		},
 		duration: {
@@ -26,11 +45,12 @@ const experiences = [
 		description: {
 			'pt-br': <>
 				<Italic>Pronto Combustíveis</Italic> é uma empresa de venda de combustíveis
-				para postos de bandeira branca. Minha função foi participar da equipe de
-				desenvolvimento do front-end de aplicativos web utilizando técnologias
-				como React, Typescript e GraphQL.
+				para postos de bandeira branca. Minha função é participar da equipe de
+				desenvolvimento front-end do sistema web interno da empresa e do aplicativo
+				externo para seus clientes, utilizando técnologias como React, Typescript e GraphQL.
 			</>,
 			'en-us': <>
+				{/* TODO - Update */}
 				<Italic>Pronto Combustíveis</Italic> is a company that sells fuel for
 				gas stations. My job was to participate in a front-end development squad for
 				fron-end web applications. Here, we worked with technologies such as React,
@@ -76,86 +96,87 @@ const experiences = [
 			'pt-br': <>
 				<Italic>Liven</Italic> é uma startup com o modelo de venture builder em São Carlos.
 				Nesta empresa, trabalhei principalmente como deselvolvedor front-end, encarregado de
-				construir uma aplicação PWA utilizando React e Redux, junto de um time composto por
+				construir um aplicativo web de banco utilizando React e Redux, junto de um time composto por
 				mais um desenvolvedor back-end e um scrum master.
 			</>,
 			'en-us': <>
+				{/* TODO - Update */}
 				<Italic>Liven</Italic> is a venture builder startup in São Carlos. There, I mainly
 				worked as a front-end developer in charge of building a PWA application using react
 				and Redux, alongside a back-end developer and a scrum master.
 			</>,
 		},
 	},
-	{
-		title: {
-			'pt-br': <>Ministrei um minicurso de React na Semcomp</>,
-			'en-us': <>Taught a React minicourse at Semcomp</>,
-		},
-		duration: {
-			'pt-br': <>01/10/2019</>,
-			'en-us': <>10/01/2019</>,
-		},
-		description: {
-			'pt-br': <>
-				Semcomp é um evento da USP que promove o conhecimento nas diversas áreas da tecnologia.
-				Neste evento lecionei em um minicurso de quatro horas para 30 pessoas sobre Web, React
-				e React Native, onde foi desenvolvido um app de quiz.
-			</>,
-			'en-us': <>
-				Semcomp is a USP event that promotes knowledge in different areas of technology.
-				At this event, I was invited to teach a four hours long minicourse about Web, React
-				and React native. About 30 people Attended the event and a quiz game app was developed.
-			</>,
-		},
-	},
-	{
-		title: {
-			'pt-br': <>Coordenador do USPCodeLab</>,
-			'en-us': <>Coordinator at USPCodeLab</>,
-		},
-		duration: {
-			'pt-br': <>2019 - 2020 (1 ano)</>,
-			'en-us': <>2019 - 2020 (1 year)</>
-		},
-		description: {
-			'pt-br': <>
-				USPCodeLab é um grupo de extensão acadêmico na Universidade de São Paulo
-				com o objetivo de ensinar e promover as mais usadas tecnologias Web. Como
-				coordenador, minhas principais responsabilidades são preparar e ministrar
-				aulas de desenvolvimento web, além de criar sites para outros grupos de extensão.
-			</>,
-			'en-us': <>
-				USPCodeLab is an extension group at the University of São Paulo,
-				with the goal to teach and promote the most used web technologies.
-				As a coordinator, my main responsibilities were to prepare and teach
-				web development classes, along with creating sites for other extension groups.
-			</>,
-		},
-	},
-	{
-		title: {
-			'pt-br': <>Participei do Agrohack (Hackaton)</>,
-			'en-us': <>Attended Agrohack (Hackaton)</>,
-		},
-		duration: {
-			'pt-br': <>02/07/2019</>,
-			'en-us': <>07/02/2019</>,
-		},
-		description: {
-			'pt-br': <>
-				Agrohack é um hackaton focado em agronomia realizado na ESALQ. Liderei uma equipe
-				que desenvolveu um aplicativo para conectar mercados e restaurantes aos mais próximos
-				produtores. O evento tinha aproximadamente 10 times, cada um com 5 - 7 membros, com
-				duração de 3 dias, no qual minha equipe alcançou a premiação.
-			</>,
-			'en-us': <>
-				Agrohack is an agronomy focused hackaton held at ESALQ. My team developed an app
-				to help local restaurants and stores to find the closest producer to them.
-				The event had about 10 teams, 5 - 7 members each, and lasted for 3 days.
-				My team was awarded.
-			</>,
-		},
-	},
+	// {
+	// 	title: {
+	// 		'pt-br': <>Ministrei um minicurso de React na Semcomp</>,
+	// 		'en-us': <>Taught a React minicourse at Semcomp</>,
+	// 	},
+	// 	duration: {
+	// 		'pt-br': <>01/10/2019</>,
+	// 		'en-us': <>10/01/2019</>,
+	// 	},
+	// 	description: {
+	// 		'pt-br': <>
+	// 			Semcomp é um evento da USP que promove o conhecimento nas diversas áreas da tecnologia.
+	// 			Neste evento lecionei em um minicurso de quatro horas para 30 pessoas sobre Web, React
+	// 			e React Native, onde foi desenvolvido um app de quiz.
+	// 		</>,
+	// 		'en-us': <>
+	// 			Semcomp is a USP event that promotes knowledge in different areas of technology.
+	// 			At this event, I was invited to teach a four hours long minicourse about Web, React
+	// 			and React native. About 30 people Attended the event and a quiz game app was developed.
+	// 		</>,
+	// 	},
+	// },
+	// {
+	// 	title: {
+	// 		'pt-br': <>Coordenador do USPCodeLab</>,
+	// 		'en-us': <>Coordinator at USPCodeLab</>,
+	// 	},
+	// 	duration: {
+	// 		'pt-br': <>2019 - 2020 (1 ano)</>,
+	// 		'en-us': <>2019 - 2020 (1 year)</>
+	// 	},
+	// 	description: {
+	// 		'pt-br': <>
+	// 			USPCodeLab é um grupo de extensão acadêmico na Universidade de São Paulo
+	// 			com o objetivo de ensinar e promover as mais usadas tecnologias Web. Como
+	// 			coordenador, minhas principais responsabilidades são preparar e ministrar
+	// 			aulas de desenvolvimento web, além de criar sites para outros grupos de extensão.
+	// 		</>,
+	// 		'en-us': <>
+	// 			USPCodeLab is an extension group at the University of São Paulo,
+	// 			with the goal to teach and promote the most used web technologies.
+	// 			As a coordinator, my main responsibilities were to prepare and teach
+	// 			web development classes, along with creating sites for other extension groups.
+	// 		</>,
+	// 	},
+	// },
+	// {
+	// 	title: {
+	// 		'pt-br': <>Participei do Agrohack (Hackaton)</>,
+	// 		'en-us': <>Attended Agrohack (Hackaton)</>,
+	// 	},
+	// 	duration: {
+	// 		'pt-br': <>02/07/2019</>,
+	// 		'en-us': <>07/02/2019</>,
+	// 	},
+	// 	description: {
+	// 		'pt-br': <>
+	// 			Agrohack é um hackaton focado em agronomia realizado na ESALQ. Liderei uma equipe
+	// 			que desenvolveu um aplicativo para conectar mercados e restaurantes aos mais próximos
+	// 			produtores. O evento tinha aproximadamente 10 times, cada um com 5 - 7 membros, com
+	// 			duração de 3 dias, no qual minha equipe alcançou a premiação.
+	// 		</>,
+	// 		'en-us': <>
+	// 			Agrohack is an agronomy focused hackaton held at ESALQ. My team developed an app
+	// 			to help local restaurants and stores to find the closest producer to them.
+	// 			The event had about 10 teams, 5 - 7 members each, and lasted for 3 days.
+	// 			My team was awarded.
+	// 		</>,
+	// 	},
+	// },
 	// {
 	// 	title: {
 	// 		'pt-br': <>Participei do Interhack (Hackaton)</>,
